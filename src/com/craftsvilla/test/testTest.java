e071111948cdfecb19b39733e5637677f08c7951
tyss <tyss@localhost>
2017-11-15 09:54:03 +0530

    updated test

---
+++ src/com/craftsvilla/test/testTest.java
@@ -0,0 +1,12 @@
+package com.craftsvilla.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testTest {
+
+	@Test
+	public void test() {
+		Reporter.log("Tesing jenkins", true);
+	}
+}
